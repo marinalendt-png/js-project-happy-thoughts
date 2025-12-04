46a3a31e261267a8326456e6726c80cc9e402900
Marina Lendt <marinalendt@gmail.com>
2025-12-04 09:30:45 +0100

    add: styling to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from "styled-components";
 import MessageForm from './components/MessageForm';
 import MessageCard from './components/MessageCard';
 import GlobalStyle from "./styles/GlobalStyle";
@@ -8,26 +9,51 @@ const App = () => {
 
   const addMessage = (text) => {
     const newMessage = {
+      id: Date.now(),
       text,
       likes: 0,
-      time: 'Just now',
+      time: Date.now(),
     };
-    setMessages([newMessage, ...messages]);
+    setMessages(prev => [newMessage, ...prev]);
   };
 
   return (
     <>
       <GlobalStyle />
-      <div className="App">
+      <AppContainer>
         <MessageForm onSend={addMessage} />
-        <div className="messages-list">
-          {messages.map((msg, index) => (
-            <MessageCard key={index} message={msg} />
+        <MessageList>
+          {messages.map((msg) => (
+            <MessageCard key={msg.id} message={msg} />
           ))}
-        </div>
-      </div>
+        </MessageList>
+      </AppContainer>
     </>
   );
 }
 
 export default App;
+
+const AppContainer = styled.section`
+  width: 100%;
+  max-width: 900px;
+  margin: 0 auto;
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const MessageList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    gap: 16px;
+  }
+`;
+
+
+
